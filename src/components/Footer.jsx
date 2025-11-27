884e087cc2d9dc248f7df35ce24e099489247202
Facundo Giuricich <facundogiuricich936@gmail.com>
2025-11-27 15:23:11 -0300

    Agrego componentes comunes: Navbar, Footer y Loading

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-zinc-950 border-t border-zinc-800 text-zinc-400 text-center py-6">
+      <p className="text-sm">
+        &copy; {currentYear}{" "}
+        <span className="text-white font-medium hover:text-red-500 transition">
+          Facundo Giuricich
+        </span>
+      </p>
+      <p className="text-xs mt-1 text-zinc-500 tracking-wide">
+        Trabajo Práctico Integrador II
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
